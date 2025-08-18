1652a7e6b9b0502d878ad1ce67155609b45ca18d
niketarora <niketarora682@gmail.com>
2025-08-18 19:10:48 +0530

    Revert "Delete Projects due directory"
    This reverts commit f968d63cbaeaceb119709db64a5b7d7a5f64b277.

---
+++ Projects due/Quiz App/script.js
@@ -0,0 +1,62 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is smallest country in the world?",
+        answers: [
+            { text: "Vatican City", correct: true},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Sri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.questions;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+    })
+}
